f1fbcf44417bd23d3514205c8a28d3a3b6168a33
NATURAL88 <88309078+NATURAL88@users.noreply.github.com>
2023-08-10 15:58:44 -0700

    NavBar added

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="flex w-full items-center justify-center border-b border-white">
+	<div class="flex w-[48rem] flex-initial flex-row items-center justify-center p-2">
+		<a href="/" class="px-2">
+			<img src="./favicon.png" alt="Github page" class="h-8 flex-none" />
+		</a>
+		<button class="px-2">
+			<img src="./favicon.png" alt="about" class="h-8 flex-none">
+		</button>
+		<h1 class="flex-1 text-center text-[32px] text-white">Heardle</h1>
+	</div>
+</div>
