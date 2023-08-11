48ad5996a69f78194068827d85a375081d81c26f
NATURAL88 <88309078+NATURAL88@users.noreply.github.com>
2023-08-10 18:37:27 -0700

    tailwind font changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'noto': ['Roboto', 'Arial', 'sans-serif']
+    },
+    fontSize: {
+      '3xl': ['32px', {
+        lineHeight: '36px',
+        letterSpacing: '-0.04em'
+      }]
+    }
   },
   plugins: [],
 }
