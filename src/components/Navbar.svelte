6cbab9e4222938cdeb1f57e426c6c4583524f6da
NATURAL88 <88309078+NATURAL88@users.noreply.github.com>
2023-08-10 22:25:38 -0700

    nav bar btns

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,14 +2,20 @@
 	import '../app.css';
 </script>
 
-<div class="flex w-full items-center justify-center border-b border-white">
+<div class="flex w-full items-center justify-center border-b border-neutral-500">
 	<div class="flex w-[48rem] flex-initial flex-row items-center justify-center p-2">
-		<a href="/" class="px-2">
-			<img src="./favicon.png" alt="Github page" class="h-8 flex-none" />
+		<a href="/" class="px-3">
+			<img src="./github-mark-white.svg" alt="Github repo" class="nav-btn" />
 		</a>
-		<button class="px-2">
-			<img src="./favicon.png" alt="about" class="h-8 flex-none">
+		<button class="px-3">
+			<img src="./info-btn.svg" alt="about" class="nav-btn" />
 		</button>
-		<h1 class="flex-1 text-center text-[32px] text-white">Heardle</h1>
+		<h1 class="flex-1 text-center font-noto text-3xl font-bold text-white">Heardle Again</h1>
+		<button class="px-3">
+			<img src="./bar-graph.svg" alt="statistics" class="nav-btn">
+		</button>
+		<button class="px-3">
+			<img src="./question-btn.svg" alt="how to play" class="nav-btn" />
+		</button>
 	</div>
 </div>
