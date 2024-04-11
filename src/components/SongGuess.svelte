01e17d03a1fb9946404001b152fb75e5ed31c640
NATURAL88 <88309078+NATURAL88@users.noreply.github.com>
2024-04-10 22:08:54 -0700

    more states for guesses

--- src/components/SongGuess.svelte
+++ src/components/SongGuess.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-	type guessState = 'nuetral' | 'wrong' | 'skipped';
+	import type { guessState } from "../app";
 	let count = 10;
 
 	export let state: guessState;
 </script>
 
-<div class="flex h-9 border border-neutral-500 p-2 items-center">
-	{#if state === 'wrong'}
-		<img src="./x-mark.svg" alt="red x mark" class="h-4" />
+<div class="flex h-9 items-center border border-neutral-500 p-2">
+	{#if state === 'nuetral'}
+		<div />
+	{:else if state === 'wrong'}
+		<img src="./x-mark.svg" alt="wrong" class="h-4" />
+	{:else if state === 'half-wrong'}
+		<img src="./yellow-x-mark.svg" alt="half wrong" class="h-4" />
+	{:else if state === 'skipped'}
+		<img src="./skipped.svg" alt="skipped" class="h-4" />
 	{/if}
 </div>
