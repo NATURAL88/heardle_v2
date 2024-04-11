9c53f027d804609935c2f5aa855a4cd88dfee96c
NATURAL88 <88309078+NATURAL88@users.noreply.github.com>
2024-04-10 22:06:58 -0700

    fixed typo

--- src/components/ModalStores.ts
+++ src/components/ModalStores.ts
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store";
 
 export const showHTPModal = writable(false);
-export const showAboutoModal = writable(false);
+export const showAboutModal = writable(false);
 export const showStatsModal = writable(false);
 
