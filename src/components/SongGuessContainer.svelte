f2c55ce7b3a03ff52d704468881ace534d0902ad
NATURAL88 <88309078+NATURAL88@users.noreply.github.com>
2024-04-10 22:07:21 -0700

    changed to iterator

--- src/components/SongGuessContainer.svelte
+++ src/components/SongGuessContainer.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import SongGuess from './SongGuess.svelte';
+	import type { SongContainerArray } from '../app';
 	// Flex expands to bottom when bottom bar added
+
+	export let guesses: SongContainerArray;
 </script>
 
-<div class="mx-auto flex w-full max-w-[600px]  grow-[2] flex-col gap-2 p-2">
-	<SongGuess state="wrong" />
-	<SongGuess state="nuetral" />
-	<SongGuess state="nuetral" />
-	<SongGuess state="nuetral" />
-	<SongGuess state="nuetral" />
-	<SongGuess state="nuetral" />
+<div class="mx-auto flex w-full max-w-[600px] grow-[2] flex-col gap-2 p-2">
+	{#each guesses as guess}
+		<SongGuess state={guess} />
+	{/each}
 </div>
