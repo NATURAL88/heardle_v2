52af6610f8c8933900298384a82a75b08a3ad6e1
NATURAL88 <88309078+NATURAL88@users.noreply.github.com>
2024-05-13 18:28:26 -0700

    oEmbed for soundcloud

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,19 +1,40 @@
-import type { PageLoad } from "./$types";
+import { stringify } from 'postcss';
+import type { PageLoad } from './$types';
 
-async function getOembed(url: string): Promise<Response> {
-    const resUrl = new URL('https://soundcloud.com/oembed');
-
-    let response = fetch(`${resUrl}?url=${url}`)
-
-    return response
+declare global {
+	var widget: object;
 }
 
+// async function getOembed(url: string): Promise<Response> {
+// 	const resUrl = new URL('https://soundcloud.com/oembed');
 
-export const load: PageLoad = async ({ params }) => {
-    const oEmbedEndpoint: string = 'https://soundcloud.com/oembed'
-    const soundcloudSongUrl: string = 'https://soundcloud.com/black-eyed-peas-official/meet-me-halfway-album-version'
-    const embedHTML = await fetch(`${oEmbedEndpoint}?url=${soundcloudSongUrl}`)
-        .then(r => r.text())
+// 	let response = fetch(`${resUrl}?url=${url}`);
 
-    return { embedHTML }
-}
+// 	return response;
+// }
+
+export const load: PageLoad = async ({ fetch, params }) => {
+	// SOUNDCLOUD RELATED
+	// // console.log(params)
+	// const oEmbedEndpoint: string = 'https://soundcloud.com/oembed';
+	// const soundcloudSongUrl: string =
+	// 	'https://soundcloud.com/black-eyed-peas-official/meet-me-halfway-album-version';
+	// const oEmbedJSON: JSON = await fetch(`${oEmbedEndpoint}?url=${soundcloudSongUrl}`).then((r) =>
+	// 	r.json()
+	// );
+	// return { oEmbedJSON };
+	// SPOTIFY
+	// var client_id = '';
+	// var client_secret = '';
+	// let token = fetch('https://accounts.spotify.com/api/token', {
+	// 	method: 'POST',
+	// 	body: new URLSearchParams({
+	// 		grant_type: 'client_credentials',
+	// 		client_id: client_id,
+	// 		client_secret: client_secret
+	// 	})
+	// })
+	// 	.then((r) => r.json())
+	// 	.then((r) => r.access_token);
+	// return { token };
+};
