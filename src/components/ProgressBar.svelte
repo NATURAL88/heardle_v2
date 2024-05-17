8ec6e0292745eb56649c76db8812c455b8c3c621
NATURAL88 <88309078+NATURAL88@users.noreply.github.com>
2024-05-16 23:13:10 -0700

    added live time

--- src/components/ProgressBar.svelte
+++ src/components/ProgressBar.svelte
@@ -1,16 +1,31 @@
+<script lang="ts">
+	import { position } from '$lib/songController';
+
+	let elapsed: Date;
+	let time: string;
+
+	$: elapsed = new Date($position);
+	$: time = `${elapsed.getMinutes().toString().padStart(2, '0')}:${elapsed
+		.getSeconds()
+		.toString()
+		.padStart(2, '0')}`;
+</script>
+
 <span class="flex flex-col items-center p-1">
-	<div class="flex w-full justify-center border-y border-neutral-500">
+	<div class="flex h-3 w-full justify-center gap-[1px] border-y border-neutral-400">
 		<!-- Progress Bar -->
-		Lorem ipsum dolor sit amet.
+		<div class="w-full max-w-xl bg-neutral-600">
+			<!-- TODO -->
+		</div>
 	</div>
 
 	<div class="m-1 flex h-12 w-full max-w-xl items-center justify-between">
-		<div>Lorem.</div>
+		<div>{time}</div>
 		<button
 			class="flex h-12 w-12 max-w-xl items-center justify-center rounded-[50%] border-2 border-white"
 		>
 			<img src="./play-button.svg" alt="play button" class="h-6 pl-2" />
 		</button>
-		<div>Lorem.</div>
+		<div>16:00</div>
 	</div>
 </span>
