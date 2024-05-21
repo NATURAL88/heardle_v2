c54d87b9fcf5fc8bb524784db3ee24621df5fb56
NATURAL88 <88309078+NATURAL88@users.noreply.github.com>
2024-05-21 23:37:03 +0000

    search bar beginning

--- src/components/ProgressBar.svelte
+++ src/components/ProgressBar.svelte
@@ -10,15 +10,15 @@
 		.toString()
 		.padStart(2, '0')}`;
 
-	let guesses = 0
+	let guesses = 0;
 
 	function increment() {
 		guesses += 1;
 		if (guesses > 5) {
-			guesses = 5
+			guesses = 5;
 		}
 	}
-	
+
 	function play() {
 		widget.playFor(guesses);
 	}
@@ -34,14 +34,17 @@
 
 	<div class="m-1 flex h-12 w-full max-w-xl items-center justify-between">
 		<div>{time}</div>
-		<button on:click={increment} class="border-white border">increment +{guesses + 1}s</button>
 		<button
+			on:click={play}
 			class="flex h-12 w-12 max-w-xl items-center justify-center rounded-[50%] border-2 border-white"
 		>
-			<button on:click={play}>
-				<img src="./play-button.svg" alt="play button" class="h-6 pl-2" />
-			</button>
+			<img src="./play-button.svg" alt="play button" class="h-6 pl-2" />
 		</button>
 		<div>16:00</div>
+	</div>
+	<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, eaque?</div>
+	<div class="m-1 flex h-12 w-full max-w-xl items-center justify-between">
+		<button on:click={increment} class="border border-white">increment +{guesses + 1}s</button>
+		<button>Submit</button>
 	</div>
 </span>
